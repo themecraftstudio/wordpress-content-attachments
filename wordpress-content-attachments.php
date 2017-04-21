bfa04447a74ec3323956a2612bb73911b80d91c6
Ettore Del Negro <ettore@themecraft.studio>
2017-04-21 18:18:59 +0200

    Add class for mime type to anchor

--- wordpress-content-attachments.php
+++ wordpress-content-attachments.php
@@ -26,28 +26,96 @@
  along with Content Attachments. If not, see LICENSE.txt .
  */
 
-require_once __DIR__ . '/vendor/autoload.php';
+add_filter('media_send_to_editor', function ($html, $send_id, $attachment) {
+	$mimeType = get_post_mime_type($attachment['id']);
 
+	if (extension_loaded('dom')) {
+		$doc = new DOMDocument();
+		$doc->loadHTML(sprintf('<?xml encoding="utf-8" ?><div>%s</div>', $html), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+		// dirty fix for encoding
+		foreach ($doc->childNodes as $item)
+			if ($item->nodeType == XML_PI_NODE)
+				$doc->removeChild($item);
+		$doc->encoding = 'UTF-8';
+
+		$query = new DOMXPath($doc);
+		$anchors = $query->query('//a[@href]');
+		foreach ( $anchors as $anchor ) {
+			/** @var $anchor DOMElement */
+			$anchor->setAttribute('class',
+				sprintf('content-attachment content-attachment-%s', str_replace(['/', '.'], '-', $mimeType)));
+		}
+
+		$html = $doc->saveHTML();
+	}
+
+	return $html;
+}, 10, 3);
 
 /**
- * Activation hook
+ * Customize DOM for post attachments
  */
-register_activation_hook( __FILE__, function () {
+add_filter( 'the_content', function ($content) {
+	$post = get_post();
 
-} );
-/**
- * Deactivation hook
- */
-register_deactivation_hook( __FILE__, function () {
+	if (extension_loaded('dom') && !(empty($content))) {
+		$iconClasses = ['icon-download-arrow'];
+		$buttonText = '&#xE2C4;';
+//		$buttonClasses = ['button'];
 
-} );
-/**
- * Uninstall hook
- */
-register_uninstall_hook(__FILE__, 'contentattachments_uninstall');
-function openidconnect_uninstall() {
-	Settings::uninstall();
-}
+		$doc = new DOMDocument();
+		$doc->loadHTML(sprintf('<?xml encoding="utf-8" ?><div>%s</div>', $content), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+		// dirty fix for encoding
+		foreach ($doc->childNodes as $item)
+			if ($item->nodeType == XML_PI_NODE)
+				$doc->removeChild($item);
+		$doc->encoding = 'UTF-8';
 
-// Register admin settings
-//Settings::register();
+		$query = new DOMXPath($doc);
+		$anchors = $query->query('//a[contains(concat(" ", @class, " "), " content-attachment ")]');
+		foreach ( $anchors as $anchor ) {
+			/** @var $anchor DOMElement */
+
+			// Skip attachment prefixed or followed by text
+			if (($anchor->previousSibling && $anchor->previousSibling->nodeType == XML_TEXT_NODE) ||
+				($anchor->nextSibling && $anchor->nextSibling->nodeType == XML_TEXT_NODE))
+				continue;
+
+			try {
+				$anchorText = '';
+				foreach ($anchor->childNodes as $child_node) {
+					/** @var $child_node DOMNode */
+					if ($child_node->nodeType == XML_TEXT_NODE)
+						$anchorText = $child_node->nodeValue;
+
+					// Remove all children but the anchor text node
+					$anchor->removeChild($child_node);
+				}
+
+				// Mime type icon
+				$icon = $doc->createElement('i');
+				$icon->setAttribute('class', sprintf('%s content-attachment-icon', implode(' ', $iconClasses)));
+
+				// Text
+				$anchorTextSpan = $doc->createElement('span', $anchorText);
+				$anchorTextSpan->setAttribute('class', 'content-attachment-name');
+
+				// Wrap icon with <span>
+				$buttonSpan = $doc->createElement('span', $buttonText);
+//				$icon->setAttribute('class', sprintf('%s content-attachment-button', implode(' ', $buttonClasses)));
+				$buttonSpan->insertBefore($icon, $buttonSpan->firstChild);
+
+				if ( $anchor->firstChild == null) {
+					$anchor->appendChild($anchorTextSpan);
+					$anchor->appendChild($buttonSpan);
+				}
+			} catch (\Exception $e) {
+				echo sprintf('<pre>%s</pre>', var_export($e, true));
+			}
+		}
+
+		$content = $doc->saveHTML();
+	}
+
+	return $content;
+});
